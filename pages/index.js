f2f1184c92059c168ac078dc6a792d43ad173607
PAVANIiii56 <pavaniseelam0@gmail.com>
2024-04-02 08:20:58 +0530

    TASK

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NotificationButton from './components/NotificationButton';
+
+const HomePage = () => {
+  return (
+    <div>
+      <center>
+      <NotificationButton /></center>
+    </div>
+  );
+};
+
+export default HomePage;
